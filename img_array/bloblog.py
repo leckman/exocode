6886204fb6691a77b387a71fd4db18575b3e9aba
Laura Eckman <eckmanlw@gmail.com>
2015-07-16 17:37:39 -0400

    sensitivities adjusted to handle FITS images acceptably

--- img_array/bloblog.py
+++ img_array/bloblog.py
@@ -1,7 +1,7 @@
 '''
 Blob detection in astronomical images using various options in scikit-image module 
 
-Laura Eckman 18/06/15
+Laura Eckman 06/15
 '''
 
 from matplotlib import pyplot as plt
@@ -9,18 +9,26 @@ from skimage.feature import blob_log
 from math import sqrt
 import numpy as np
 
-def blobl(image,out_file='',display=False,fmt='png'):
+def blobl(image,out_file='',display=False):
     '''
     LaPlacian of Gaussian implementation of scikit-image blob detection program
     Args:
       image: numpy array
       out_file: str representing save location of processed image (if not empty)
       display: bool to display image as matplotlib figure
-      fmt: file extension, default 'png', alternatively 'FITS'
-    Returns dict of detected blobs (fmt: {radius:position tuple}
+    Returns dict of detected blobs (fmt: {position tuple:radius})
     '''
+    dimensions = [len(image),len(image[0])]
+    min_sigma = 3.
+    max_sigma = 30.
+    num_sigma = 20.
+    threshold = 12.
+    overlap = 1.0
+    log_scale = False
 
-    blobs_log = blob_log(image, max_sigma=30, num_sigma=10, threshold=.1)
+    blobs_log = blob_log(image,min_sigma,max_sigma,num_sigma,
+        threshold,overlap,log_scale)
+
     if blobs_log.any():
         blobs_log[:,2] = blobs_log[:,2] * sqrt(2)
 
@@ -28,7 +36,10 @@ def blobl(image,out_file='',display=False,fmt='png'):
     blob_dict = {}
     for blob in blobs_log:
         y, x, r = blob
-        blob_dict[r] = (x,y)
+        if ((r>.25*dimensions[1] or r>.25*dimensions[1]) and 
+            (y in (0,dimensions[0]) or x in (0,dimensions[0]))):
+            continue
+        blob_dict[(x,y)] = r
         c = plt.Circle((x,y), r, color='b', linewidth=2, fill=False)
         center = plt.Circle((x,y),.5,color ='b',linewidth=0.5,fill=True)
         fig.axes.add_patch(c)
